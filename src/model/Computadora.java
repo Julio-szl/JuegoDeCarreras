8bb60ddc49d85a38e736c0711c0f8fff89511ff4
Julio-szl <julio.szl591@gmail.com>
2026-09-09 22:58:46 -0600

    Se creo la clase padre base de jugador y tambien se crearon las clases hijas (jugador 1 y 2 y el de la computadora)

--- src/model/Computadora.java
+++ src/model/Computadora.java
@@ -1,6 +1,48 @@
 package model;
 
-public class Computadora {
+import java.util.Random;
 
-    private puntuacion;
+public class Computadora extends Jugador {
+
+    private String dificultad;
+    private Random random;
+    
+    public Computadora(String name, Vehiculo vehiculo, String dificultad) {
+        super(name, vehiculo);
+        this.dificultad = dificultad;
+    }
+
+    // CON ESTE METODO AUN FALTA REVISAR LAS PROBABILIDADES DE MOVIMIENTO, YA QUE NO SE SI ESTAN BIEN PUESTAZZZZZZZZ
+    @Override
+    public void decidirMovimiento() {
+        random = new Random();
+        double probabilidad = random.nextDouble();
+        double distancia = 0.0;
+
+        switch (dificultad) {
+            case "Fácil" -> {
+                if (probabilidad < 0.7) {
+                    distancia = 1;
+                } else {
+                    distancia = 1;
+                }
+            }
+            case "Normal" -> {
+                if (probabilidad < 0.5) {
+                    distancia = 1;
+                } else {
+                    distancia = 1;
+                }
+            }
+            case "Difícil" -> {
+                if (probabilidad < 0.3) {
+                    distancia = 1;
+                } else {
+                    distancia = 1;
+                }
+            }
+            default -> throw new IllegalArgumentException("Dificultad no válida: " + dificultad);
+        }
+        avanzar(distancia);
+    }
 }
---
+++ src/model/JugadorDos.java
@@ -0,0 +1,27 @@
+package model;
+
+public class JugadorDos extends Jugador {
+
+    private int teclaArriba;
+    private int teclaAbajo;
+
+    public JugadorDos(String name, Vehiculo vehiculo, int teclaArriba, int teclaAbajo) {
+        super(name, vehiculo);
+        this.teclaArriba = teclaArriba;
+        this.teclaAbajo = teclaAbajo;
+    }
+
+    private void manejarTecla(int tecla) {
+        if (tecla == teclaArriba) {
+            avanzar(1); // Distancia 1 porque se mueve un carril hacia arriba
+        } else if (tecla == teclaAbajo) {
+            avanzar(1); // Distancia 1 porque se mueve un carril hacia abajo
+        }
+    }
+
+    @Override
+    public void decidirMovimiento() {
+        // Aquí se implementaría la lógica para decidir el movimiento del jugador uno
+        // NO HAY CODIGO DE MOMENTO!!!!!!!!!!!   
+    }
+}
