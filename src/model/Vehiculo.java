e206c021842dedc3f4d5eb44799dc6b7e493822f
alexvrro <octubrealex141999@gmail.com>
2026-09-09 19:10:09 -0600

    arreglo e implementacion en clase padre

--- src/model/Vehiculo.java
+++ src/model/Vehiculo.java
@@ -1,75 +1,120 @@
 package model;
 
+import excepciones.DatosVehiculoException;
+
 public abstract class Vehiculo {
 
     private String nombre;
-    private int velocidadMaxima;
-    private int aceleracion;
-    private int manejo;
-    private int nitro;
-    private int posicion;
+    private double velocidad;
+    private double velocidadMaxima;
+    private double aceleracion;
+    private double distanciaRecorrida;
+    private String rutaImagen;
 
-    public Vehiculo(String nombre, int velocidadMaxima, int aceleracion,
-                    int manejo, int nitro) {
+    public Vehiculo(
+            String nombre,
+            double velocidadMaxima,
+            double aceleracion,
+            String rutaImagen
+    ) throws DatosVehiculoException {
+
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new DatosVehiculoException(
+                    "El nombre del vehículo no puede estar vacío."
+            );
+        }
+
+        if (velocidadMaxima <= 0) {
+            throw new DatosVehiculoException(
+                    "La velocidad máxima debe ser mayor que cero."
+            );
+        }
+
+        if (aceleracion <= 0) {
+            throw new DatosVehiculoException(
+                    "La aceleración debe ser mayor que cero."
+            );
+        }
 
         this.nombre = nombre;
+        this.velocidad = 0;
         this.velocidadMaxima = velocidadMaxima;
         this.aceleracion = aceleracion;
-        this.manejo = manejo;
-        this.nitro = nitro;
-        this.posicion = 0;
+        this.distanciaRecorrida = 0;
+        this.rutaImagen = rutaImagen;
+    }
+
+    public void acelerar() {
+        velocidad += aceleracion;
+
+        if (velocidad > velocidadMaxima) {
+            velocidad = velocidadMaxima;
+        }
+    }
+
+    public void avanzar() {
+        distanciaRecorrida += velocidad;
+    }
+
+    public void frenar() {
+        velocidad -= aceleracion;
+
+        if (velocidad < 0) {
+            velocidad = 0;
+        }
+    }
+
+    public void reiniciar() {
+        velocidad = 0;
+        distanciaRecorrida = 0;
+    }
+
+    public abstract void usarHabilidad();
+
+    public abstract String getTipo();
+
+    public String mostrarInformacion() {
+        return "Nombre: " + nombre
+                + "\nTipo: " + getTipo()
+                + "\nVelocidad máxima: " + velocidadMaxima
+                + "\nAceleración: " + aceleracion;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public int getVelocidadMaxima() {
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public double getVelocidadMaxima() {
         return velocidadMaxima;
     }
 
-    public int getAceleracion() {
+    public double getAceleracion() {
         return aceleracion;
     }
 
-    public int getManejo() {
-        return manejo;
+    public double getDistanciaRecorrida() {
+        return distanciaRecorrida;
     }
 
-    public int getNitro() {
-        return nitro;
+    public String getRutaImagen() {
+        return rutaImagen;
     }
 
-    public int getPosicion() {
-        return posicion;
-    }
-
-    public void avanzarPosicion(int cantidad) {
-        posicion += cantidad;
-    }
-
-    public void reiniciarPosicion() {
-        posicion = 0;
-    }
-
-    public abstract int avanzar();
-
-    public int usarNitro() {
-
-        if (nitro > 0) {
-            nitro--;
-            return 8;
+    protected void setVelocidad(double velocidad) {
+        if (velocidad < 0) {
+            this.velocidad = 0;
+        } else if (velocidad > velocidadMaxima) {
+            this.velocidad = velocidadMaxima;
+        } else {
+            this.velocidad = velocidad;
         }
-
-        return 0;
     }
 
-    public String mostrarEstadisticas() {
-
-        return "Vehiculo: " + nombre +
-                "\nVelocidad maxima: " + velocidadMaxima +
-                "\nAceleracion: " + aceleracion +
-                "\nManejo: " + manejo +
-                "\nNitros disponibles: " + nitro;
+    protected void setDistanciaRecorrida(double distanciaRecorrida) {
+        this.distanciaRecorrida = Math.max(0, distanciaRecorrida);
     }
 }
